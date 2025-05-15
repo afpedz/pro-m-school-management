5cee8963c521030ea0e3e61d49636d12cc982a0e
afpedz <pedrazaalfrancis@gmail.com>
2025-05-16 02:18:27 +0800

    feat: update Carousel component items and remove unused Tailwind CSS import

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -12,54 +12,67 @@
 </template>
 
 <script lang="ts" setup>
-const items = [{
-    name: 'Sébastien Chopin',
-    to: 'https://github.com/Atinux',
-    avatar: { src: 'https://ipx.nuxt.com/f_auto,s_192x192/gh_avatar/atinux' }
-}, {
-    name: 'Sébastien Chopin',
-    to: 'https://github.com/Atinux',
-    avatar: { src: 'https://ipx.nuxt.com/f_auto,s_192x192/gh_avatar/atinux' }
-},
-{
-    name: 'Sébastien Chopin',
-    to: 'https://github.com/Atinux',
-    avatar: { src: 'https://ipx.nuxt.com/f_auto,s_192x192/gh_avatar/atinux' }
-},
-{
-    name: 'Sébastien Chopin',
-    to: 'https://github.com/Atinux',
-    avatar: { src: 'https://ipx.nuxt.com/f_auto,s_192x192/gh_avatar/atinux' }
-},
-{
-    name: 'Sébastien Chopin',
-    to: 'https://github.com/Atinux',
-    avatar: { src: 'https://ipx.nuxt.com/f_auto,s_192x192/gh_avatar/atinux' }
-}, {
-    name: 'Pooya Parsa',
-    to: 'https://github.com/pi0',
-    avatar: { src: 'https://ipx.nuxt.com/f_auto,s_192x192/gh_avatar/pi0' }
-}, {
-    name: 'Daniel Roe',
-    to: 'https://github.com/danielroe',
-    avatar: { src: 'https://ipx.nuxt.com/f_auto,s_192x192/gh_avatar/danielroe' }
-}, {
-    name: 'Anthony Fu',
-    to: 'https://github.com/antfu',
-    avatar: { src: 'https://ipx.nuxt.com/f_auto,s_192x192/gh_avatar/antfu' },
-},
-{
-    name: 'Anthony Fu',
-    to: 'https://github.com/antfu',
-    avatar: { src: 'https://ipx.nuxt.com/f_auto,s_192x192/gh_avatar/antfu' },
+const items = [
+    {
+        name: 'Anthony Fu',
+        to: 'https://github.com/antfu',
+        avatar: { src: 'https://ipx.nuxt.com/f_auto,s_192x192/gh_avatar/antfu' },
 
-},
-{
-    name: 'Anthony Fu',
-    to: 'https://github.com/antfu',
-    avatar: { src: 'https://ipx.nuxt.com/f_auto,s_192x192/gh_avatar/antfu' },
+    },
+    {
+        name: 'Anthony Fu',
+        to: 'https://github.com/antfu',
+        avatar: { src: 'https://ipx.nuxt.com/f_auto,s_192x192/gh_avatar/antfu' },
 
-}]
+    },
+    {
+        name: 'Anthony Fu',
+        to: 'https://github.com/antfu',
+        avatar: { src: 'https://ipx.nuxt.com/f_auto,s_192x192/gh_avatar/antfu' },
+
+    },
+    {
+        name: 'Anthony Fu',
+        to: 'https://github.com/antfu',
+        avatar: { src: 'https://ipx.nuxt.com/f_auto,s_192x192/gh_avatar/antfu' },
+
+    },
+    {
+        name: 'Anthony Fu',
+        to: 'https://github.com/antfu',
+        avatar: { src: 'https://ipx.nuxt.com/f_auto,s_192x192/gh_avatar/antfu' },
+
+    },
+    {
+        name: 'Anthony Fu',
+        to: 'https://github.com/antfu',
+        avatar: { src: 'https://ipx.nuxt.com/f_auto,s_192x192/gh_avatar/antfu' },
+
+    },
+    {
+        name: 'Anthony Fu',
+        to: 'https://github.com/antfu',
+        avatar: { src: 'https://ipx.nuxt.com/f_auto,s_192x192/gh_avatar/antfu' },
+
+    },
+    {
+        name: 'Anthony Fu',
+        to: 'https://github.com/antfu',
+        avatar: { src: 'https://ipx.nuxt.com/f_auto,s_192x192/gh_avatar/antfu' },
+
+    },
+    {
+        name: 'Anthony Fu',
+        to: 'https://github.com/antfu',
+        avatar: { src: 'https://ipx.nuxt.com/f_auto,s_192x192/gh_avatar/antfu' },
+
+    },
+    {
+        name: 'Anthony Fu',
+        to: 'https://github.com/antfu',
+        avatar: { src: 'https://ipx.nuxt.com/f_auto,s_192x192/gh_avatar/antfu' },
+
+    }]
 </script>
 
 <style></style>
